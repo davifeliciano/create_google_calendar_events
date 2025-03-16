b8f4d4521e7c3c2c3db226fea4e1d121c7daf895
Davi Feliciano <dfeliciano37@gmail.com>
2025-03-16 19:41:40 -0300

    feat: use argparse for cli args validation

--- create_google_calendar_events.py
+++ create_google_calendar_events.py
@@ -1,16 +1,86 @@
-import sys
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import pickle
 import os.path
+import argparse
 
 # Se modificarmos as SCOPES, deletar o arquivo token.pickle.
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
 
 
-def main():
-    """Cria eventos de dia inteiro no Google Calendar em uma agenda específica."""
+def valid_month(value):
+    """Valida o valor do mês."""
+    month_int = int(value)
+
+    if month_int < 1 or month_int > 12:
+        raise argparse.ArgumentTypeError(
+            f"{value} não é um mês válido. Deve estar entre 1 e 12."
+        )
+
+    return month_int
+
+
+def valid_year(value):
+    """Valida o valor do ano."""
+    year_int = int(value)
+
+    if year_int < 1990 or year_int > 2999:
+        raise argparse.ArgumentTypeError(
+            f"{value} não é um ano válido. Deve estar entre 1990 e 2999."
+        )
+
+    return year_int
+
+
+def parse_args():
+    """Analisa os argumentos da linha de comando."""
+    parser = argparse.ArgumentParser(
+        description="Cria eventos de dia inteiro no Google Calendar em uma agenda específica nos dias do mês e ano informados."
+    )
+
+    parser.add_argument(
+        "-d",
+        "--days",
+        nargs="+",
+        type=int,
+        required=True,
+        help="Dias do mês separados por espaços",
+    )
+
+    parser.add_argument(
+        "-m",
+        "--month",
+        type=valid_month,
+        required=True,
+        help="Número do mês (1-12)",
+    )
+
+    parser.add_argument(
+        "-y",
+        "--year",
+        type=valid_year,
+        required=True,
+        help="Ano (1990-2999)",
+    )
+
+    parser.add_argument(
+        "-n", "--name", type=str, required=True, help="Nome dos eventos"
+    )
+
+    parser.add_argument(
+        "-c",
+        "--calendar",
+        type=str,
+        required=True,
+        help="Nome da agenda do Google Calendar na qual os eventos serão criados",
+    )
+
+    return parser.parse_args()
+
+
+def get_credentials():
+    """Obtém credenciais do usuário."""
     creds = None
 
     # O arquivo token.pickle armazena os tokens de acesso e atualização do usuário,
@@ -31,35 +101,7 @@ def main():
         with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
 
-    service = build("calendar", "v3", credentials=creds)
-
-    # Obtenha o id da agenda "Trabalho"
-    calendar_id = get_calendar_id(service, "Trabalho")
-
-    if not calendar_id:
-        print('Agenda "Trabalho" não encontrada.')
-        return
-
-    # Obtenha argumentos de linha de comando
-    dias = [int(dia) for dia in sys.argv[1].split(",")]
-    mes = int(sys.argv[2])
-    ano = int(sys.argv[3])
-    descricao = sys.argv[4]
-
-    for dia in dias:
-        # Cria o evento
-        event = {
-            "summary": descricao,
-            "start": {
-                "date": f"{ano:04d}-{mes:02d}-{dia:02d}",
-            },
-            "end": {
-                "date": f"{ano:04d}-{mes:02d}-{dia:02d}",
-            },
-        }
-
-        event = service.events().insert(calendarId=calendar_id, body=event).execute()
-        print(f'Evento criado: {event.get("htmlLink")}')
+    return creds
 
 
 def get_calendar_id(service, calendar_name):
@@ -78,6 +120,32 @@ def get_calendar_id(service, calendar_name):
             break
 
     return None
+
+
+def main():
+    """Cria eventos de dia inteiro no Google Calendar em uma agenda específica."""
+    args = parse_args()
+    creds = get_credentials()
+    service = build("calendar", "v3", credentials=creds)
+    calendar_id = get_calendar_id(service, args.calendar)
+
+    if not calendar_id:
+        print(f"Agenda `{args.calendar}` não encontrada.")
+        return
+
+    for day in args.days:
+        event = {
+            "summary": args.name,
+            "start": {
+                "date": f"{args.year:04d}-{args.month:02d}-{day:02d}",
+            },
+            "end": {
+                "date": f"{args.year:04d}-{args.month:02d}-{day:02d}",
+            },
+        }
+
+        event = service.events().insert(calendarId=calendar_id, body=event).execute()
+        print(f'Evento criado: {event.get("htmlLink")}')
 
 
 if __name__ == "__main__":
